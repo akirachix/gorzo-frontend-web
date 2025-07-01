52e4623c06bc6427d2ac72f3308f29608aa47b3a
student <aberasemhal@gamil.com>
2025-07-01 12:44:33 +0300

    created a sign up and sign in pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LocationPrompt from './location/Location';
+
 import SignUp from './signUp/signUp';
 import InputField from './sharedcomponent/inputField';
 import SignIn from './signIn/signIn';
@@ -7,9 +7,9 @@ import SignIn from './signIn/signIn';
 function App() {
   return (
     <div className="App">
-      {/* <LocationPrompt /> */}
-       {/* <SignUp></SignUp> */}
-       <SignIn></SignIn>
+     
+       <SignUp></SignUp>
+       {/* <SignIn></SignIn> */}
     </div>
        
   );
