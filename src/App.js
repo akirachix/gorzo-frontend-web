7c6d39be6f7ce8adda7842058668dff5d4941b02
meronkahsay <kahsaymeront@gmail.com>
2025-07-24 13:42:44 +0300

    Added some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-  //  import SplashScreen from './OnBoarding/SplashScreen';
-  //  import Welcome from './OnBoarding/WelcomeScreen';
-  //  import OrderManagement from './OnBoarding/OrderManagement';
-  //  import InventoryManagement from './OnBoarding/InventoryManagement';
-  //  import SalesTracking from './OnBoarding/SalesTracking';
-  //  import GetStarted from './OnBoarding/GetStarted';
   import Users from './FetchUsers';
    import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
    import '@fontsource/manrope';
-  //  import Home from './HomePage/Home';
    import Sidebar from './SharedComponent/SideBar';
    const Orders = () => <div></div>;
    const Sales = () => <div></div>;
@@ -28,14 +21,9 @@ import { Outlet } from 'react-router-dom';
      return (
        <Router>
          <Routes>
-           {/* <Route path="/" element={<SplashScreen />} /> */}
-           {/* <Route path="/welcome" element={<Welcome />} />
-           <Route path="/inventory" element={<InventoryManagement />} />
-           <Route path="/order" element={<OrderManagement />} />
-           <Route path="/getStarted" element={<GetStarted />} />
-           <Route path="/sales" element={<SalesTracking />} /> */}
+         
            <Route element={<MainLayout />}>
-             {/* <Route path="/home" element={<Home />} /> */}
+  
              <Route path="/orders" element={<Orders />} />
              <Route path="/users" element={<Users />} />
              <Route path="/salestracking" element={<Sales />} />
